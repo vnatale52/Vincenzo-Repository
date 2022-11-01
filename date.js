6da6c83c96e63e329b7e9e9d0829bb31032c0409
vnatale52 <43297347+vnatale52@users.noreply.github.com>
2022-11-01 11:24:57 -0300

    Update date.js

--- date.js
+++ date.js
@@ -1,5 +1,5 @@
 const date = new Date();
 const n = date.toDateString();
 const time = date.toLocaleTimeString();
-document.write('Date: ' + n + "   -   ");
+document.write('Date: ' + n + " -- ");
 document.write('Time: ' + time);;
